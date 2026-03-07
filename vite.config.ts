288f9c976c3656efdf2831687e1ac7d53c46180f
mhieu <mhieudzvcl@gmail.com>
2026-03-08 02:34:19 +0700

    simple change

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/my-portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
